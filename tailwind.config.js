e3d77058296bc5a77eb748f8eee2c108695b6721
Luizbernardi <luizbernardi37@gmail.com>
2024-09-19 19:51:16 -0300

    tailwind e cores config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                greenlight: "#00953b",
+                redligth: "#f6323e",
+                begligth: "#ffecc1",
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
